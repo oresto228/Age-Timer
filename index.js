ba029a10f2acb0891c62addfcc8173440bf5f6d4
Krendelyk <65673523+oresto228@users.noreply.github.com>
2024-02-22 14:23:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function startAgeTimer() {
+    document.getElementsByClassName('container')[0].style= "opacity:0";
+    const birthdateInput = document.getElementById('birthdate');
+    const birthdate = new Date(birthdateInput.value);
+    const ageElement = document.getElementById('age');
+  
+    if (!birthdate || isNaN(birthdate.getTime())) {
+      alert("Please enter a valid birthdate.");
+      return;
+    }
+  
+    const timerId = setInterval(updateAge, 100);
+  
+    function updateAge() {
+      const now = new Date();
+      const ageInMilliseconds = now - birthdate;
+      const fractionalYears = (ageInMilliseconds / 31556900000).toFixed(9);
+    ageElement.textContent = `Age ${fractionalYears}`
+    // document.getElementsByClassName('container')[0].style= "opacity:0";
+    }
+    updateAge();
+  }
+  
