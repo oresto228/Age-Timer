3c11f60157b61a081a2438b2f48e316fb4e4ef82
Orest <orest361@gmail.com>
2024-02-26 13:40:21 +0200

    bug fix

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 function startAgeTimer() {
-    document.getElementsByClassName('container')[0].style= "opacity:0";
-    const birthdateInput = document.getElementById('birthdate');
-    const birthdate = new Date(birthdateInput.value);
-    const ageElement = document.getElementById('age');
-  
-    if (!birthdate || isNaN(birthdate.getTime())) {
-      alert("Please enter a valid birthdate.");
-      return;
-    }
-  
-    const timerId = setInterval(updateAge, 100);
-  
-    function updateAge() {
-      const now = new Date();
-      const ageInMilliseconds = now - birthdate;
-      const fractionalYears = (ageInMilliseconds / 31556900000).toFixed(9);
-    ageElement.textContent = `Age ${fractionalYears}`
-    // document.getElementsByClassName('container')[0].style= "opacity:0";
-    }
-    updateAge();
+  const birthdateInput = document.getElementById('birthdate');
+  const birthdate = new Date(birthdateInput.value);
+  const ageElement = document.getElementById('age');
+
+  if (!birthdate || isNaN(birthdate.getTime())) {
+    alert("Please enter a valid birthdate.");
+    return;
   }
-  
+  document.getElementsByClassName('container')[0].style= "opacity:0";
+  const timerId = setInterval(updateAge, 100);
+
+  function updateAge() {
+    const now = new Date();
+    const ageInMilliseconds = now - birthdate;
+    const fractionalYears = (ageInMilliseconds / 31556900000).toFixed(9);
+  ageElement.textContent = `Age ${fractionalYears}`
+  }
+  updateAge();
+}
